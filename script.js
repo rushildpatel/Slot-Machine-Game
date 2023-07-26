a5c85d5a8e4d87b028e667e531428be464ed42da
Your Name <you@example.com>
2023-07-27 02:59:09 +0530

    slot machine slots changing and spinning emojis randomly

---
+++ script.js
@@ -0,0 +1,15 @@
+let value1 = document.getElementById("value1");
+let value2 = document.getElementById("value2");
+let value3 = document.getElementById("value3");
+
+let values = ["😭", "😃", "😑", "😶‍🌫️", "😔", "😎", "😇"];
+
+function getRandomValue() {
+  return values[parseInt(Math.floor(Math.random() * 7))];
+}
+
+setInterval(() => {
+  value1.innerText = getRandomValue();
+  value2.innerText = getRandomValue();
+  value3.innerText = getRandomValue();
+}, 300);
